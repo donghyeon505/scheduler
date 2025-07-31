1181d4564a2d6919223685fd8b97c32bf0b8a947
donghyeon505 <ehdgus458@gmail.com>
2025-07-31 14:25:08 +0900

    feat: 일정 조회 및 선택 일정 조회 추가 (Lv2 완성)

--- src/main/java/com/example/scheduler/service/SchedulerService.java
+++ src/main/java/com/example/scheduler/service/SchedulerService.java
@@ -5,8 +5,13 @@ import com.example.scheduler.dto.SchedulerResponse;
 import com.example.scheduler.entity.Scheduler;
 import com.example.scheduler.repository.SchedulerRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -36,5 +41,51 @@ public class SchedulerService {
                 savedScheduler.getCreatedAt(),
                 savedScheduler.getModifiedAt()
         );
+    }
+
+    // 일정 조회
+    @Transactional (readOnly = true)
+    public List<SchedulerResponse> findAll() {
+
+        // Repository 에서 정보 조회
+        List<Scheduler> schedules = schedulerRepository.findAll();
+
+        // 응답 객체 리스트 초기화
+        List<SchedulerResponse> schedulerResponses = new ArrayList<>();
+
+        // 받은 정보를 Response 객체로 변환해서 저장
+        for (Scheduler scheduler : schedules) {
+            schedulerResponses.add(new SchedulerResponse(
+                    scheduler.getId(),
+                    scheduler.getTitle(),
+                    scheduler.getContents(),
+                    scheduler.getWriter(),
+                    scheduler.getCreatedAt(),
+                    scheduler.getModifiedAt()
+            ));
+        }
+
+        // 조회(변환)된 리스트 반환
+        return schedulerResponses;
+    }
+
+    // 선택 일정 조회
+    @Transactional (readOnly = true)
+    public SchedulerResponse findById(Long id) {
+
+        // 일정 찾기 (null 값이면 NOT_FOUND)
+        Scheduler schedule = schedulerRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "일정을 찾을 수 없습니다"));
+
+        // 조회된 일정 반환
+        return new SchedulerResponse(
+                schedule.getId(),
+                schedule.getTitle(),
+                schedule.getContents(),
+                schedule.getWriter(),
+                schedule.getCreatedAt(),
+                schedule.getModifiedAt()
+        );
+
     }
 }
