c61c8df9f5c86487e82361238a01f1973b62c1bc
donghyeon505 <ehdgus458@gmail.com>
2025-08-01 10:20:43 +0900

    feat: 댓글 리스트 조회 시 일정 ID 기준 필터링 기능 추가

--- src/main/java/com/example/scheduler/repository/CommentRepository.java
+++ src/main/java/com/example/scheduler/repository/CommentRepository.java
@@ -3,7 +3,10 @@ package com.example.scheduler.repository;
 import com.example.scheduler.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     Long countBySchedulerId(Long schedulerId);
+    List<Comment> findAllBySchedulerId(Long schedulerId);
 }
